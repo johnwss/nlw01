c973b6792e589f0e815893a05db178b6bcfb44d1
JohnnyW10Desktop <johnny.wesley@gmail.com>
2020-10-12 19:43:02 -0300

    escrevendoJs

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,1 +1,13 @@
-fetch()
+/*
+
+const urlParaFetch = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos"
+fetch(urlParaFetch).then(function funcaoresolvedora(dadosAlocadosAqui){
+    return dadosAlocadosAqui.json() }).then(function funcaoTudoResolvido(dadosProntos){
+        console.log(dadosProntos)
+    }) */
+
+
+    
+
+document.querySelector("select[name=uf]").addEventListener("change", ()=> {console.log("showw")
+})
