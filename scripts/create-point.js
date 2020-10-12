b16464ea15d084d890c8e238b1d99b51bf852eea
Johnny Santos <johnny@MacBook-Pro-de-Johnny.local>
2020-10-12 20:01:56 -0300

    continuandoJS

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -6,8 +6,15 @@ fetch(urlParaFetch).then(function funcaoresolvedora(dadosAlocadosAqui){
         console.log(dadosProntos)
     }) */
 
+function popularAsUF(){
+    const ufSelect = document.querySelector("select[name=uf]")
+    const urlParaFetch = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos"
+    fetch(urlParaFetch).then()
+}
 
     
 
-document.querySelector("select[name=uf]").addEventListener("change", ()=> {console.log("showw")
-})
+document
+.querySelector("select[name=uf]").addEventListener("change", ()=> {console.log("showw") })
+
+
