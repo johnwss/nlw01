2b12210b58ffc42adae49eef6eb2a32b68245e0b
JohnnyW10Desktop <johnny.wesley@gmail.com>
2020-10-12 22:26:25 -0300

    Rendering

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -6,15 +6,29 @@ fetch(urlParaFetch).then(function funcaoresolvedora(dadosAlocadosAqui){
         console.log(dadosProntos)
     }) */
 
-function populandoAsUF(){
+function populandoAUF(){
     
     const ufSelect = document.querySelector("select[name=uf]")
     const urlParaFetch = "https://servicodados.ibge.gov.br/api/v1/localidades/distritos"
 
-    fetch(urlParaFetch).then(dadosAlocadosAqui => 
-        dadosAlocadosAqui.json()
-    ).then()
+    fetch(urlParaFetch).
+        then(dadosAlocadosAqui => dadosAlocadosAqui.json())
+        .then(estadosBrRecebidos => {
+
+        //document.querySelector("select[name=uf]").innerHTML, ou ...(abaixo)
+        //ufSelect.innerHTML = ufSelect.innerHTML + `<option value = "1">Valor</option>`
+
+        //for(const i of estadosBrRecebidos){
+
+        for(i of estadosBrRecebidos){
+            ufSelect.innerHTML +=  `<option value = "${i.id}">${i.nome}</option>`
+        } 
+        })
+
+        
 }
+
+populandoAUF()
 
     
 
